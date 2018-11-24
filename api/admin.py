4a28fd70f4a1c84984fb569c9002c34e0d44c925
Noah Francis <noahwalugembe@gmail.com>
2018-11-25 02:43:26 +0300

    Added model to admin

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
 
+from api.models.dashboards import Dashboard
 # Register your models here.
+
+admin.site.register(Dashboard)
