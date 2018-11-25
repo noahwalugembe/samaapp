20517087adf1f6fb1dcc4d082b1f3f308438f12c
Noah Francis <noahwalugembe@gmail.com>
2018-11-25 23:26:31 +0300

    Updated test file

--- api/tests.py
+++ api/tests.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+# /api/tests.py
+
+
+from api.models.dashboards import Dashboard
+
+
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the dashboard model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.dashboard_name = "outbox"
+        self.dashboard = Dashboard(name=self.dashboard_name)
+
+    def test_model_can_create_a_dashboard(self):
+        """Test the dashboard model can create a dashboard."""
+        old_count = Dashboard.objects.count()
+        self.dashboard.save()
+        new_count = Dashboard.objects.count()
+        self.assertNotEqual(old_count, new_count)
